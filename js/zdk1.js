c53593484fd54117b73d154a3da3b738fd427ad9
ToniGrbic <tonigrbic.5@gmail.com>
2024-02-02 21:19:31 +0100

    alert insted of confirm

--- js/zdk1.js
+++ js/zdk1.js
@@ -1,33 +1,35 @@
-let products = [];
+function zdk1() {
+  let products = [];
 
-while (true) {
-  let name = prompt("Enter product name:");
-  let price = Number(prompt("Enter product price:"));
-  console.log(price);
-  if (isNaN(price) || price <= 0) {
-    if (!confirm("Invalid price input, try again?")) break;
-    continue;
+  while (true) {
+    let name = prompt("Enter product name:");
+    let price = Number(prompt("Enter product price:"));
+    console.log(price);
+    if (isNaN(price) || price <= 0) {
+      alert("Invalid price input, try again!");
+      continue;
+    }
+
+    let color = prompt("Enter product color:");
+    products.push({ name, price, color });
+
+    if (!confirm("Do you want to continue input new product?")) break;
   }
 
-  let color = prompt("Enter product color:");
-  products.push({ name, price, color });
+  let total_price = products.reduce((sum, product) => sum + product.price, 0);
+  let average_price = total_price / products.length;
 
-  if (!confirm("Do you want to continue input new product?")) break;
+  let deviations = products.map((product) => ({
+    name: product.name,
+    deviation: Math.round(Math.abs(product.price - average_price) * 100) / 100,
+  }));
+
+  let productWithMaxDeviation = deviations.reduce((prev, current) =>
+    prev.deviation > current.deviation ? prev : current
+  );
+
+  console.log("Products: ", products);
+  console.log("Average price:", average_price.toFixed(2));
+  console.log("Deviations: ", deviations);
+  console.log("Product with max deviation: ", productWithMaxDeviation);
 }
-
-let total_price = products.reduce((sum, product) => sum + product.price, 0);
-let average_price = total_price / products.length;
-
-let deviations = products.map((product) => ({
-  name: product.name,
-  deviation: Math.round(Math.abs(product.price - average_price) * 100) / 100,
-}));
-
-let productWithMaxDeviation = deviations.reduce((prev, current) =>
-  prev.deviation > current.deviation ? prev : current
-);
-
-console.log("Products: ", products);
-console.log("Average price:", average_price.toFixed(2));
-console.log("Deviations: ", deviations);
-console.log("Product with max deviation: ", productWithMaxDeviation);
